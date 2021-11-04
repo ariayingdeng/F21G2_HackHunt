b56e09737c1272f140be721d7758760122408ea4
Ying Deng <dengy9@student.douglascollege.ca>
2021-11-03 23:46:25 -0700

    Added code for testing the connection of parse server

--- app/src/main/java/com/example/f21g2_hackhunt/MainActivity.java
+++ app/src/main/java/com/example/f21g2_hackhunt/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.f21g2_hackhunt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParseObject parseObject = new ParseObject("Test");
+        parseObject.put("Firstname", "Ying");
+        parseObject.put("Lastname", "Deng");
+        parseObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.i("Connection", "Successful");
+                }
+                else {
+                    Log.i("Connection", "Unsuccessful");
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
     }
 }
