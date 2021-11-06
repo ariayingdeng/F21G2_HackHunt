d26ee183bc54ec39f7b7d13a94401cf4acb28432
Jin Chin <chinj7@student.douglascollege.ca>
2021-11-05 22:25:56 -0700

    Commit for pull

--- app/src/main/java/com/example/f21g2_hackhunt/MainActivity.java
+++ app/src/main/java/com/example/f21g2_hackhunt/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
@@ -44,9 +45,7 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            }
 //        });
-
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
-
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
 
     }
