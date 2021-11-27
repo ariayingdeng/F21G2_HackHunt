8d06cbb9b8f10091e3fd403c35cec98a834f78fb
Jin Chin <chinj7@student.douglascollege.ca>
2021-11-26 19:48:16 -0800

    Added CommentDao and CommentAdapterForSql for transfering comment feature from parse to sql

--- app/src/main/java/com/example/f21g2_hackhunt/adapters/CommentAdapterForSql.java
+++ app/src/main/java/com/example/f21g2_hackhunt/adapters/CommentAdapterForSql.java
@@ -1,2 +1,49 @@
-package com.example.f21g2_hackhunt.adapters;public class CommentAdapterForSql {
+package com.example.f21g2_hackhunt.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.f21g2_hackhunt.R;
+import com.example.f21g2_hackhunt.model.Comment;
+
+import java.util.List;
+
+public class CommentAdapterForSql extends BaseAdapter {
+
+    List<Comment> commentList;
+
+    public CommentAdapterForSql(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+
+    @Override
+    public int getCount() {
+        return commentList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return commentList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view == null){
+            view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_comment,viewGroup,false);
+        }
+        TextView textViewCommenter = view.findViewById(R.id.textViewCommenter);
+        TextView textViewComment = view.findViewById(R.id.textViewComment);
+        textViewCommenter.setText(commentList.get(i).getCommenter());
+        textViewComment.setText(commentList.get(i).getComments());
+
+        return view;
+    }
 }
--- app/src/main/java/com/example/f21g2_hackhunt/interfaces/CommentDao.java
+++ app/src/main/java/com/example/f21g2_hackhunt/interfaces/CommentDao.java
@@ -1,2 +1,24 @@
-package com.example.f21g2_hackhunt.interfaces;public interface CommentDao {
+package com.example.f21g2_hackhunt.interfaces;
+
+import static androidx.room.OnConflictStrategy.IGNORE;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.f21g2_hackhunt.model.Comment;
+
+import java.util.List;
+
+@Dao
+public interface CommentDao {
+    @Insert(onConflict = IGNORE)
+    void insertComment(Comment comment);
+
+    @Insert(onConflict = IGNORE)
+    void insertCommentList(List<Comment> comment);
+
+    @Query("SELECT * FROM comments WHERE postId = :pId")
+    List<Comment> getAllCommentsForPost(String pId);
+
+
 }
